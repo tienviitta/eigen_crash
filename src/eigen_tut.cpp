33b08b8e48375d75b3b46febad7148b3e27560d5
Väisänen, Petri <Petri.Vaisanen@nordicsemi.no>
2023-01-30 15:01:19 +0200

    Setup clangd and formatting

--- src/eigen_tut.cpp
+++ src/eigen_tut.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
 #include <Eigen/Dense>
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-void run_eigen_tut()
-{
+void run_eigen_tut() {
 
     // MatrixXd m = MatrixXd::Random(3, 3);
     // m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
@@ -19,29 +18,21 @@ void run_eigen_tut()
     //           << m * v << std::endl;
 
     Eigen::Matrix2d a;
-    a << 1, 2,
-        3, 4;
+    a << 1, 2, 3, 4;
     Eigen::MatrixXd b(2, 2);
-    b << 2, 3,
-        1, 4;
-    std::cout << "a + b =\n"
-              << a + b << std::endl;
-    std::cout << "a - b =\n"
-              << a - b << std::endl;
+    b << 2, 3, 1, 4;
+    std::cout << "a + b =\n" << a + b << std::endl;
+    std::cout << "a - b =\n" << a - b << std::endl;
     std::cout << "Doing a += b;" << std::endl;
     a += b;
-    std::cout << "Now a =\n"
-              << a << std::endl;
+    std::cout << "Now a =\n" << a << std::endl;
     Eigen::Vector3d v(1, 2, 3);
     Eigen::Vector3d w(1, 0, 0);
-    std::cout << "-v + w - v =\n"
-              << -v + w - v << std::endl;
+    std::cout << "-v + w - v =\n" << -v + w - v << std::endl;
 
     Eigen::Matrix2d mat;
-    mat << 1, 2,
-        3, 4;
-    std::cout << "mat =" << std::endl
-              << mat << std::endl;
+    mat << 1, 2, 3, 4;
+    std::cout << "mat =" << std::endl << mat << std::endl;
     std::cout << "Here is mat.sum():       " << mat.sum() << std::endl;
     std::cout << "Here is mat.prod():      " << mat.prod() << std::endl;
     std::cout << "Here is mat.mean():      " << mat.mean() << std::endl;
@@ -51,8 +42,7 @@ void run_eigen_tut()
 
     Eigen::ArrayXXf aa(2, 2);
 
-    aa << 1, 2,
-        3, 4;
+    aa << 1, 2, 3, 4;
 
     std::cout << "(a > 0).all()   = " << (aa > 0).all() << std::endl;
     std::cout << "(a > 0).any()   = " << (aa > 0).any() << std::endl;
@@ -63,10 +53,8 @@ void run_eigen_tut()
     std::cout << "(a > 2).count() = " << (aa > 2).count() << std::endl;
 
     Eigen::ArrayXXi A = Eigen::ArrayXXi::Random(4, 4).abs();
-    std::cout << "Here is the initial matrix A:\n"
-         << A << "\n";
+    std::cout << "Here is the initial matrix A:\n" << A << "\n";
     for (auto row : A.rowwise())
         std::sort(row.begin(), row.end());
-    std::cout << "Here is the sorted matrix A:\n"
-         << A << "\n";
+    std::cout << "Here is the sorted matrix A:\n" << A << "\n";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <Eigen/Dense>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
@@ -6,22 +8,36 @@
 using json = nlohmann::json;
 using namespace std;
 
-int main()
-{
+// https://zed0.co.uk/clang-format-configurator/
+
+int main() {
+
+    // Read params
     std::ifstream params_f("sims/params.json");
 
-    if (params_f)
-    {
+    if (params_f) {
         // Parse JSON
         json p_top = json::parse(params_f);
         // cout << setw(4) << p_top << "\n";
         // Get sub dicts
         auto p_sys = p_top["sys"];
-        cout << p_sys << "\n";
+        cout << "p_sys:\n" << p_sys << "\n";
         // cout << p_sys["bw"].get<int>() << "\n";
         auto p_ssb = p_sys["ssb"];
-        cout << p_ssb << "\n";
+        cout << "p_ssb:\n" << p_ssb << "\n";
         auto p_bwp = p_sys["bwp"];
-        cout << p_bwp << "\n";
+        cout << "p_bwp:\n" << p_bwp << "\n";
     }
+
+    Eigen::Matrix2d a;
+    a << 1, 2, 3, 4;
+    Eigen::MatrixXd b(2, 2);
+    b << 2, 3, 1, 4;
+    std::cout << "a + b =\n" << a + b << std::endl;
+    std::cout << "a - b =\n" << a - b << std::endl;
+    std::cout << "Doing a += b;" << std::endl;
+    a += b;
+    std::cout << "Now a =\n" << a << std::endl;
+
+    return EXIT_SUCCESS;
 }
